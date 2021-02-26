b6bda262095fd2861619fc256105af2c585d2da7
mrlandfried <30705227+mrlandfried@users.noreply.github.com>
2021-02-26 10:10:24 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import * #gives us access to everything in the Tkinter class
+
+root = Tk() #gives us a blank canvas object to work with
+
+
+
+
+
+
+mainloop() #causes the windows to display on the screen until program closes
